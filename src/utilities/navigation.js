c6b333caadab633625be7e20acba6bb0e6c7b89c
sk2111 <sathisht1996@gmail.com>
2021-08-20 18:14:56 +0530

    feat : Add navigation constants for NavHeader
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/utilities/navigation.js
+++ src/utilities/navigation.js
@@ -1,28 +1,60 @@
+import { ReactComponent as AccountSvg } from 'assets/account.svg';
+import { ReactComponent as BillingSvg } from 'assets/billing.svg';
+import { ReactComponent as ShipppingSvg } from 'assets/shipping.svg';
+import { ReactComponent as PaymentSvg } from 'assets/payment.svg';
+
 export const ACCOUNT_PAGE = 'accountInfo';
 export const BILLING_PAGE = 'billingInfo';
 export const SHIPPING_PAGE = 'shippingInfo';
 export const PAYMENT_PAGE = 'paymentInfo';
 export const SIGNUP_SUCCESS = 'signupSuccess';
 
-export const PAGE_LIST = [
+export const navHeaderList = [
+  { id: 1, 
+    title: 'Account', 
+    isActive: false, 
+    Icon: AccountSvg 
+  },
+  {
+    id: 2,
+    title: 'Billing Info',
+    isActive: false,
+    Icon: BillingSvg,
+  },
+  {
+    id: 3,
+    title: 'Shipping Info',
+    isActive: false,
+    Icon: ShipppingSvg,
+  },
+  {
+    id: 4,
+    title: 'Payment',
+    isActive: false,
+    Icon: PaymentSvg,
+  },
+];
+
+export const navButtonList = [
   ACCOUNT_PAGE,
   BILLING_PAGE,
   SHIPPING_PAGE,
   PAYMENT_PAGE,
 ];
 
-export const INIT_PAGE = PAGE_LIST[0];
-
-const getCurrentPageIdx = (currPage) => {
-  return PAGE_LIST.findIndex((page) => page === currPage);
+const navigationMap = {
+  [ACCOUNT_PAGE]: { next: BILLING_PAGE, prev: null },
+  [BILLING_PAGE]: { next: SHIPPING_PAGE, prev: ACCOUNT_PAGE },
+  [SHIPPING_PAGE]: { next: PAYMENT_PAGE, prev: BILLING_PAGE },
+  [PAYMENT_PAGE]: { next: null, prev: SHIPPING_PAGE },
 };
 
+export const [startingPage] = navButtonList;
+
 export const getNextPage = (currPage) => {
-  const currIdx = getCurrentPageIdx(currPage);
-  return PAGE_LIST[currIdx + 1] ?? PAGE_LIST[0];
+  return navigationMap[currPage].next;
 };
 
 export const getPreviousPage = (currPage) => {
-  const currIdx = getCurrentPageIdx(currPage);
-  return PAGE_LIST[currIdx - 1] ?? PAGE_LIST[PAGE_LIST.length - 1];
+  return navigationMap[currPage].prev;
 };
